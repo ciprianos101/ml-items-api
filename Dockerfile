9448dfbad71f583fc7f025518535a6ef893b333a
Lucas <lucaseduardo101@gmail.com>
2025-08-23 15:55:51 -0300

    Adicionando Dockerfile e ajustando file para para ler do classpath

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# Etapa 1: Build da aplicação
+FROM gradle:8.7.0-jdk17 AS build
+WORKDIR /app
+COPY . .
+RUN ./gradlew clean bootJar --no-daemon
+
+# Etapa 2: Imagem final para execução
+FROM eclipse-temurin:17-jre
+WORKDIR /app
+COPY --from=build /app/build/libs/*.jar app.jar
+
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/kotlin/com/example/itemdetail/adapter/persistence/ItemFileRepository.kt
+++ src/main/kotlin/com/example/itemdetail/adapter/persistence/ItemFileRepository.kt
@@ -3,15 +3,14 @@ package com.example.itemdetail.adapter.persistence
 import com.example.itemdetail.domain.model.Item
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Repository
-import java.io.File
 
 @Repository
 class ItemFileRepository {
     fun getItems(): List<Item> {
-        val file = File(javaClass.classLoader.getResource("data/items.json")!!.file)
-        return jacksonObjectMapper().readValue(file)
+        val inputStream = javaClass.classLoader.getResourceAsStream("data/items.json")
+            ?: throw IllegalStateException("Arquivo data/items.json não encontrado")
+        return jacksonObjectMapper().readValue(inputStream)
     }
 
     fun findById(id: String): Item? = getItems().find { it.id == id }
