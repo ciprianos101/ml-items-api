d9385effeadac81279ac8d3c5b5327e1a31eb9d7
Lucas <lucaseduardo101@gmail.com>
2025-08-26 01:38:43 -0300

    removendo dao

--- src/main/kotlin/com/lucasoliveira/itemdetail/domain/usecase/itemsdetails/port/ItemRepository.kt
+++ src/main/kotlin/com/lucasoliveira/itemdetail/domain/usecase/itemsdetails/port/ItemRepository.kt
@@ -3,6 +3,6 @@ package com.lucasoliveira.itemdetail.domain.usecase.itemsdetails.port
 import com.lucasoliveira.itemdetail.domain.model.Item
 import java.util.UUID
 
-interface ItemDao {
-    suspend fun byId(id: UUID): Item?
+interface ItemRepository {
+    suspend fun findById(id: UUID): Item?
 }
